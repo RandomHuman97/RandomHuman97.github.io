af1de8759a63cefa043446e3ca356896b06966ac
RandomHuman97 <103230029+RandomHuman97@users.noreply.github.com>
2025-04-13 11:57:47 -0400

    pls work

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import Home from "./pages/home/home";
 import "./index.css"
 import Posts from "./pages/posts/posts";
@@ -8,11 +8,11 @@ import RenderPost from "./pages/renderpost/renderpost";
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/posts" element={<Posts/>} />
       <Route path="/posts/:postName" element={<RenderPost />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
