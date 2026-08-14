e97265166bae1ff7fc35ee5c9774483ad6153337
Nicholas Karneyenka <103230029+RandomHuman97@users.noreply.github.com>
2026-08-14 13:00:22 -0400

    local FONTS!!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -8,5 +8,14 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: 'Inter',
+    cssVariable: '--font-inter',
+    weights: ['100 900'],
+    styles: ['normal', 'italic'],
+    subsets: ['latin'],
+
+  }]
 
 });
