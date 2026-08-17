95f3e40bc6d91a11cbc62f8ca8cac35d212c53b5
Nicholas Karneyenka <103230029+RandomHuman97@users.noreply.github.com>
2026-08-17 15:02:39 -0400

    favicons querima bobo ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import favicons from 'astro-favicons';
+
 export default defineConfig({
   site: "https://human97.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [mdx(), sitemap(), favicons()],
   fonts: [{
     provider: fontProviders.fontsource(),
     name: 'Inter',
